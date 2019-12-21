4083c1329bbcbb9368af4980d72bc0d4d0b0ce86
Pary Ly <paleepary@aol.com>
2019-12-20 17:50:13 -0800

    was able to append the user input to api link

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,20 @@
 // DATA THAT HOLDS THE USE DATA INPUT
 
-var friendsArray = []
+var friendsArray = [
+    {
+        friendName: "Joe",
+        friendImage: "Rogan",
+        question1: 5,
+        question2: 1,
+        question3: 2,
+        question4: 2,
+        question5: 3,
+        question6: 1,
+        question7: 5,
+        question8: 2,
+        question9: 4,
+        question10: 1
+    }
+]
 
 module.exports = friendsArray
